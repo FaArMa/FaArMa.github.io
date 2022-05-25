8f718c617b27ca1cc2ef8b43ff6660a7763d1040
Fabricio <101414179+FaArMa@users.noreply.github.com>
2022-05-25 10:39:25 -0300

    Agregado aviso sobre este repositorio Comentario en HTML y mensaje en la Consola

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+function AltoPolicia() {
+    console.log("%c🚨 ALTO, POLICÍA! 👮", "color: red; font-size: 50px; font-weight: bold;");
+    console.log("%c¿Sabe qué no está permitido ir por esta ruta?", "font-size: 18px;");
+    console.log("%cSi quiere ver el código fuente, visite https://github.com/FaArMa/FaArMa.github.io", "font-size: 18px;");
+    return;
+}
+
+window.onload = AltoPolicia();
